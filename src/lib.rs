7f2d7874e9d608b710344b0597f23f11a35ed14b
Lokathor <zefria@gmail.com>
2023-03-10 12:00:12 -0700

    basic data types, we'll probably need just a few more, not many more.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,53 @@
 #![no_std]
+#![allow(nonstandard_style)]
+
+/// Red/Green/Blue color, encoded as sRGB values, `u8` per channel.
+///
+/// * **GL:** `internalFormat=GL_SRGB8`, `format=GL_RGB`,
+///   `type=GL_UNSIGNED_BYTE`
+/// * **VK:** `VK_FORMAT_R8G8B8_SRGB`
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(feature = "bytemuck", derive(bytemuck::Pod, bytemuck::Zeroable))]
+#[repr(C)]
+pub struct r8g8b8Srgb {
+  pub r: u8,
+  pub g: u8,
+  pub b: u8,
+}
+
+/// Red/Green/Blue color, encoded as linear normalized values, `u8` per channel.
+///
+/// Note that 8 bits is too little precision to encode linear colors well, so
+/// this format is *inherently* a not-great option for doing color work.
+///
+/// * **GL:** `internalFormat=GL_RGB8`, `format=GL_RGB`, `type=GL_UNSIGNED_BYTE`
+/// * **VK:** `VK_FORMAT_R8G8B8_UNORM`
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(feature = "bytemuck", derive(bytemuck::Pod, bytemuck::Zeroable))]
+#[repr(C)]
+pub struct r8g8b8Unorm {
+  pub r: u8,
+  pub g: u8,
+  pub b: u8,
+}
+
+/// * **VK:** `VK_FORMAT_R32G32B32_SFLOAT`
+#[derive(Debug, Clone, Copy, Default, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "bytemuck", derive(bytemuck::Pod, bytemuck::Zeroable))]
+#[repr(C)]
+pub struct r32g32b32Sfloat {
+  pub r: f32,
+  pub g: f32,
+  pub b: f32,
+}
+
+/// * **VK:** `VK_FORMAT_R32G32B32A32_SFLOAT`
+#[derive(Debug, Clone, Copy, Default, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "bytemuck", derive(bytemuck::Pod, bytemuck::Zeroable))]
+#[repr(C)]
+pub struct r32g32b32a32Sfloat {
+  pub r: f32,
+  pub g: f32,
+  pub b: f32,
+  pub a: f32,
+}
